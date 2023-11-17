755127e25eb771f0e6b8767392791c07b32bc251
somaiaelbaradey <somayaelbaradey@gmail.com>
2023-11-17 19:38:27 +0200

    sol: BE task

--- service/src/data.js
+++ service/src/data.js
@@ -1,36 +1,46 @@
-const fs = require('fs');
+const fs = require("fs");
 
 todos = {};
 
 const data = {
-  getAllTodos: () => {
-    return todos;
-  },
+    getAllTodos: () => {
+        return todos;
+    },
 
-  deleteTodoById: (id) => {
-    const deletedTodo = todos[id];
-    delete todos[id];
-    data.save();
-    return deletedTodo;
-  },
+    deleteTodoById: (id) => {
+        const deletedTodo = todos[id];
+        delete todos[id];
+        data.save();
+        return deletedTodo;
+    },
 
-  load: () => {
-    try {
-      const raw = fs.readFileSync('data/todos.json');
-      todos = JSON.parse(raw);
-    } catch (error) {
-      console.error('Error loading JSON data:', error);
-    }
-  },
+    load: () => {
+        try {
+            const raw = fs.readFileSync("data/todos.json");
+            todos = JSON.parse(raw);
+        } catch (error) {
+            console.error("Error loading JSON data:", error);
+        }
+    },
 
-  save: () => {
-    try {
-      const raw = JSON.stringify(todos, null, 2);
-      fs.writeFileSync('data/todos.json', raw);
-    } catch (error) {
-      console.error('Error saving JSON data:', error);
-    }
-  }
-}
+    create: (response) => {
+        try {
+            const todoId = Object.keys(todos).length + 1;
+            todos[todoId] = { ...response, id: todoId };
+            data.save();
+        } catch (err) {
+            console.error("Error creating TODO:", err);
+        }
+    },
+
+    save: () => {
+        try {
+            const raw = JSON.stringify(todos, null, 2);
+            fs.writeFileSync("data/todos.json", raw);
+        } catch (error) {
+            console.error("Error saving JSON data:", error);
+        }
+    },
+};
 
 module.exports = { data };
--- service/src/routes.js
+++ service/src/routes.js
@@ -1,14 +1,18 @@
-const { Router } = require('express');
-const { data } = require('./data.js');
+const { Router } = require("express");
+const { data } = require("./data.js");
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-  res.json(data.getAllTodos());
+router.get("/", (_req, res) => {
+    res.json(data.getAllTodos());
 });
 
-router.delete('/:id', (req, res) => {
-  res.json(data.deleteTodoById(req.params.id))
+router.delete("/:id", (req, res) => {
+    res.json(data.deleteTodoById(req.params.id));
+});
+
+router.post("/todo", (req, res) => {
+    res.json(data.create(req.body));
 });
 
 module.exports = { router };
